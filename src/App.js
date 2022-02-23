81921c05f1241c68816f1e75a8d509a22ab160b2
Vladimir Levadny <v.levadny@rambler.ru>
2022-02-23 05:09:31 +0300

    end

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Layout />}>
 						<Route index element={<Homepage />}/>
-						<Route path='about' element={<Aboutpage />}/>
+						<Route path='about/*' element={<Aboutpage />}>
+							<Route path='contacts' element={<p>Our contacts</p>} />
+							<Route path='team' element={<p>Our team</p>} />
+						</Route>
 						<Route path='about-us' element={<Navigate to='/about' replace />}/>
 						<Route path='posts' element={<Blogpage />}/>
 						<Route path='posts/:id' element={<Singlepage />}/>
--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
 
 const Aboutpage = () => {
     return (
         <div>
             <h1>About us</h1>
             <p>This is a demo website about React-Router-dom library</p>
+            <ul>
+                <li><Link to='contacts' >Our contacts</Link></li>
+                <li><Link to='team' >Our team</Link></li>
+            </ul>
+
+            <Outlet />
+
+            {/*<Routes>*/}
+            {/*    <Route path='contacts' element={<p>Our contacts</p>} />*/}
+            {/*    <Route path='team' element={<p>Our team</p>} />*/}
+            {/*</Routes>*/}
         </div>
     );
 };
