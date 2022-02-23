f9fc64278f914dbb7dcf941b89961ddb2c165f09
Vladimir Levadny <v.levadny@rambler.ru>
2022-02-23 04:27:04 +0300

    add blog filter

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import {Link, useLocation } from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
+import { BlogFilter } from "../components/BlogFilter";
 
 const Blogpage = () => {
     const [ posts, setPosts ] = useState([]);
-    console.log(useLocation())
+    const [ searchParams, setSearchParams ] = useSearchParams();
+
+    const postQuery = searchParams.get('post') || ''
+    // URL.ru/posts?post=abc&data=
+    const latest = searchParams.has('latest');
+
+    const startsForms = latest ? 80 : 1;
+
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -15,12 +23,19 @@ const Blogpage = () => {
     return (
         <div>
             <h1>Our news</h1>
-            <Link to='/posts/new'>Add new post</Link>
-            {posts.length > 0 && posts.map(post => (
-                <Link key={post.id} to={`/posts/${post.id}`}>
-                    <li>{ post.title }</li>
-                </Link>
-            ))}
+
+            <BlogFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams} />
+
+            <Link to="/posts/new" style={{margin: '1rem 0', display: 'inline-block'}}>Add new post</Link>
+            {
+                posts.filter(
+                    post => post.title.includes(postQuery) && post.id >= startsForms
+                ).map(post => (
+                    <Link key={post.id} to={`/posts/${post.id}`}>
+                        <li>{post.title}</li>
+                    </Link>
+                ))
+            }
         </div>
     );
 };
